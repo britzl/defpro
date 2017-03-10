0a478f7b685eae7fde8c31c8d6c74a10c6f6ba6f
Björn Ritzl <bjorn.ritzl@king.com>
2017-03-10 10:40:02 +0100

    Changed from localhost to 127.0.0.1

--- example/commandline/capture.lua
+++ example/commandline/capture.lua
@@ -4,7 +4,7 @@ local reporter = require "defpro.reporter"
 local waiting = true
 
 -- get profiler data async
-profiler.capture(10, "localhost", function(frames)
+	profiler.capture(10, "127.0.0.1", function(frames)
 	reporter.print_scope_data(frames[1])
 	reporter.print_sample_data(frames[1])
 	reporter.print_counters_data(frames[1])
